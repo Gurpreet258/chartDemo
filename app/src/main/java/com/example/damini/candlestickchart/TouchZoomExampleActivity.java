91a0dc5e0d7d77759effc6d2dbed4208cfa8ccd7
gurpreet <gurpreet258@gmail.com>
2016-06-07 15:53:01 +0530

    demo

--- app/src/main/java/com/example/damini/candlestickchart/TouchZoomExampleActivity.java
+++ app/src/main/java/com/example/damini/candlestickchart/TouchZoomExampleActivity.java
@@ -1,15 +1,23 @@
 package com.example.damini.candlestickchart;
-import java.text.DecimalFormat;
-import java.text.FieldPosition;
-import java.text.NumberFormat;
-import java.text.ParsePosition;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+/*
+ * Copyright 2015 AndroidPlot.com
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,35 +29,25 @@ import android.widget.Button;
 import com.androidplot.Plot;
 import com.androidplot.xy.BoundaryMode;
 import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
+
+import java.text.DecimalFormat;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 /***********************************
  * @author David Buezas (david.buezas at gmail.com)
  * Feel free to copy, modify and use the source as it fits you.
  * 09/27/2012 nfellows - updated for 0.5.1 and made a few simplifications
  */
-import java.text.DecimalFormat;
-import java.util.Random;
-
-import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.PointF;
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.widget.Button;
-
-import com.androidplot.Plot;
-import com.androidplot.xy.BoundaryMode;
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYPlot;
-
-
-public class TouchAndZoomActivity extends Activity implements OnTouchListener {
+public class TouchZoomExampleActivity extends Activity implements OnTouchListener {
     private static final int SERIES_SIZE = 200;
     private XYPlot mySimpleXYPlot;
     private Button resetButton;
@@ -69,31 +67,33 @@ public class TouchAndZoomActivity extends Activity implements OnTouchListener {
         mySimpleXYPlot.getGraphWidget().setRangeValueFormat(
                 new DecimalFormat("#####"));
         mySimpleXYPlot.getGraphWidget().setDomainValueFormat(
-                new DecimalFormat("#####.#"));
+                new DecimalFormat("#####.##"));
         mySimpleXYPlot.getGraphWidget().setRangeTickLabelWidth(25);
+
         mySimpleXYPlot.setRangeLabel("");
         mySimpleXYPlot.setDomainLabel("");
 
         mySimpleXYPlot.setBorderStyle(Plot.BorderStyle.NONE, null, null);
+        mySimpleXYPlot.getGraphWidget().setRangeTick(false);
+        Paint paint=new Paint();
+        paint.setColor(Color.TRANSPARENT);
+        mySimpleXYPlot.setGridPadding(50,50,50,50);
+
         //mySimpleXYPlot.disableAllMarkup();
         series = new SimpleXYSeries[4];
         int scale = 1;
-        for (int i = 0; i < 4; i++, scale *= 5) {
-            series[i] = new SimpleXYSeries("S" + i);
-            populateSeries(series[i], scale);
-        }
+        ArrayList<Integer> alValues=new ArrayList();
+        alValues.addAll(Arrays.asList(14000, 200, 300, 12200, 12000,12500, 200, 300, 12200, 5000,14000, 200, 300, 12200, 5000,14000, 200, 300, 12200, 5000));
+        alValues.addAll(Arrays.asList(14000, 200, 300, 12200, 5000,14000, 200, 300, 12200, 5000,14000, 200, 300, 12200, 5000,14000, 200, 300, 12200, 5000));
+
+        series[3]= new SimpleXYSeries(alValues,
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "");
+        mySimpleXYPlot.setRangeBoundaries(0, Collections.max(alValues)+mySimpleXYPlot.getRangeStepValue(),BoundaryMode.FIXED);
         mySimpleXYPlot.addSeries(series[3],
-                new LineAndPointFormatter(Color.rgb(50, 0, 0), null,
-                        Color.rgb(100, 0, 0), null));
-        mySimpleXYPlot.addSeries(series[2],
-                new LineAndPointFormatter(Color.rgb(50, 50, 0), null,
-                        Color.rgb(100, 100, 0), null));
-        mySimpleXYPlot.addSeries(series[1],
-                new LineAndPointFormatter(Color.rgb(0, 50, 0), null,
-                        Color.rgb(0, 100, 0), null));
-        mySimpleXYPlot.addSeries(series[0],
-                new LineAndPointFormatter(Color.rgb(0, 0, 0), null,
-                        Color.rgb(0, 0, 150), null));
+                new LineAndPointFormatter(Color.rgb(50, 150, 0), Color.rgb(0, 100, 0),
+                        null, new PointLabelFormatter(Color.BLACK)));
+
+
         mySimpleXYPlot.redraw();
         mySimpleXYPlot.calculateMinMaxVals();
         minXY = new PointF(mySimpleXYPlot.getCalculatedMinX().floatValue(),
@@ -121,6 +121,8 @@ public class TouchAndZoomActivity extends Activity implements OnTouchListener {
 
     @Override
     public boolean onTouch(View arg0, MotionEvent event) {
+
+
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN: // Start gesture
                 firstFinger = new PointF(event.getX(), event.getY());
@@ -161,6 +163,7 @@ public class TouchAndZoomActivity extends Activity implements OnTouchListener {
     }
 
     private void zoom(float scale) {
+        Log.w("tag",String.valueOf(scale));
         float domainSpan = maxXY.x - minXY.x;
         float domainMidPoint = maxXY.x - domainSpan / 2.0f;
         float offset = domainSpan * scale / 2.0f;
@@ -170,7 +173,7 @@ public class TouchAndZoomActivity extends Activity implements OnTouchListener {
 
         minXY.x = Math.min(minXY.x, series[3].getX(series[3].size() - 3)
                 .floatValue());
-        maxXY.x = Math.max(maxXY.x, series[0].getX(1).floatValue());
+        maxXY.x = Math.max(maxXY.x, series[3].getX(1).floatValue());
         clampToDomainBounds(domainSpan);
     }
 
@@ -184,7 +187,7 @@ public class TouchAndZoomActivity extends Activity implements OnTouchListener {
     }
 
     private void clampToDomainBounds(float domainSpan) {
-        float leftBoundary = series[0].getX(0).floatValue();
+        float leftBoundary = series[3].getX(0).floatValue();
         float rightBoundary = series[3].getX(series[3].size() - 1).floatValue();
         // enforce left scroll boundary:
         if (minXY.x < leftBoundary) {
